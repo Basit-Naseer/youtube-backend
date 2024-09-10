27a9db6592438633ece00fc547b548c7571f5e88
basit1234 <basitnaseer@codistan.org>
2024-09-10 16:32:14 +0500

    utils file created ApiError & ApiResponse & asyncHandler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import connectBD from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-  path: './env'
-})
+  path: "./env",
+});
 
 connectBD()
+  .then(() => {
+    app.on("Error", (error) => {
+      console.log("Error occur in server side", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoBD connection failed !!!!", err);
+  });

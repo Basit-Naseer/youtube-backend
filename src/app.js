384ff605ceb06d320196dea72f88cd8cb63bf8bb
basit1234 <basitnaseer@codistan.org>
2024-09-13 12:42:17 +0500

    create user conttroller & create router in registerUser

--- src/app.js
+++ src/app.js
@@ -1,21 +1,29 @@
-import express from 'express'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-}))
+  })
+);
 
-app.use(express.json({
-limit: "20kb"
-}))
+app.use(
+  express.json({
+    limit: "20kb",
+  })
+);
 
-app.use(express.urlencoded({extended: true, limit: "20kb"}))
+app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
-app.use(express.static("public"))
-app.use(cookieParser())
+// import routes
+import userRouter from "./routes/user.routes.js";
 
-export { app }
+app.use("/api/v1/users", userRouter);
+
+export { app };

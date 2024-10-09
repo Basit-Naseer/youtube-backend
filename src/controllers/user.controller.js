891572bcae6ee46b3b6b75e1dfdc3bb0ea54fd73
basit1234 <basitnaseer@codistan.org>
2024-10-09 11:55:04 +0500

    bugs fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,21 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "hello",
-  });
+  // get user details from frontend
+  // validation - not empty
+  // check if user already exit: username & email
+  // check for image, check for avatar
+  // upload them to cloudinary, avatar
+  // create user object - create entry in db
+  // remove password and refresh token field from response
+  // check for user creation
+  // return res
+
+  const { lastName, email, userName, password } = req.body;
+  console.log("Email", email);
 });
+
+if ([lastName, email, userName, password]) {
+}
 
 export { registerUser };
